e45cefe6587234809cb7a81309f3d94253e32665
Rodolfo Kirch Veiga <rodolfoksveiga@gmail.com>
2021-10-22 21:29:39 +0200

    validate data in the backend

--- backend/src/Controller/PostsController.php
+++ backend/src/Controller/PostsController.php
@@ -7,6 +7,8 @@ use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PostsController extends AbstractController
 {
@@ -24,12 +26,23 @@ class PostsController extends AbstractController
         ]);
     }
 
-    public function add(Request $request): Response
+    public function add(Request $request, ValidatorInterface $validator): Response
     {
         $post = (new Posts())
             ->setModifiedAt(new DateTime('now'))
             ->setTitle($request->request->get('title'))
             ->setBody($request->request->get('body'));
+        $error = $validator->validate($post);
+
+        if (count($error) > 0) {
+            $errorMessages = [];
+
+            /** @var Constraint $violantion */
+            foreach($error as $violation) {
+                $errorMessages[] = $violation->getPropertyPath() . ": " . $violation->getMessage();
+            }
+            return $this->json(['success' => false, 'error' => $errorMessages], 400);
+        }
 
         $em = $this->getDoctrine()->getManager();
         $em->persist($post);
@@ -39,6 +52,5 @@ class PostsController extends AbstractController
             return $this->json(['success' => true, 'post' => $post], 201);
         }
 
-        return $this->json(['success' => false, 400]);
     }
 }
--- backend/src/Entity/Posts.php
+++ backend/src/Entity/Posts.php
@@ -6,6 +6,7 @@ use App\Repository\PostsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PostsRepository::class)
@@ -21,16 +22,19 @@ class Posts
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\NotBlank
      */
     private $modifiedAt;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
      */
     private $body;
 
