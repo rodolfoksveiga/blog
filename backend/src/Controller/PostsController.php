b03494faa364d7f3893aacc72c312a73ec18241e
Rodolfo Kirch Veiga <rodolfoksveiga@gmail.com>
2021-10-22 20:43:15 +0200

    create add method in PostsController

--- backend/src/Controller/PostsController.php
+++ backend/src/Controller/PostsController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Posts;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
@@ -13,9 +14,31 @@ class PostsController extends AbstractController
     {
         $posts = $this->getDoctrine()->getRepository(Posts::class)->findAll();
         
+        if (!$posts) {
+            return $this->json(['success' => false], 404);
+        }
+
         return $this->json([
             'success' => true,
             'posts' => $posts,
         ]);
+    }
+
+    public function add(Request $request): Response
+    {
+        $post = (new Posts())
+            ->setModifiedAt(new DateTime('now'))
+            ->setTitle($request->request->get('title'))
+            ->setBody($request->request->get('body'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+
+        if ($post->getId()) {
+            return $this->json(['success' => true, 'post' => $post], 201);
+        }
+
+        return $this->json(['success' => false, 400]);
     }
 }
